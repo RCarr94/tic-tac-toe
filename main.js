46d67c0b2d4d0fa3e569e24c161b76d0aabe447b
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2022-12-12 12:00:55 -0500

    Finished step 6

--- main.js
+++ main.js
@@ -37,11 +37,16 @@ const squares = document.querySelectorAll("td");
 // 4) Upon loading the app should:
 // 	4.1) Initialize the state variables:
 // 		4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
-board = [null, null, null, null, null, null, null, null, null];
-// 		4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
-whosTurn = 1;
-// 		4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie.
-winner = null;
+function iniVariables() {
+  // 		4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
+  board = [null, null, null, null, null, null, null, null, null];
+  // 		4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
+  whosTurn = 1;
+  // 		4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie.
+  winner = null;
+}
+iniVariables();
+
 
 // 	4.2) Render those state variables to the page:
 // 		4.2.1) Render the board:
@@ -113,3 +118,8 @@ function renderBoard() {
 
 // 6) Handle a player clicking the replay button:
 // 	6.1) Do steps 4.1 (initialize the state variables) and 4.2 (render).
+document.getElementById("reset-btn").addEventListener("click", function() {
+  iniVariables();
+  renderBoard();
+  document.querySelector("h1").innerHTML = "Tic-Tac-Toe";
+});
